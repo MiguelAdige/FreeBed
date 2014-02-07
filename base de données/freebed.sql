b0eb55f2a2f2f340ea6a829cb3ca3ab2882dfbe7
MiguelAdige <mixta971@hotmail.fr>
2014-02-07 17:13:35 +0100

    BDD
    Fichier sql

---
+++ base de données/freebed.sql
@@ -0,0 +1,149 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `freebed` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `freebed` ;
+
+-- -----------------------------------------------------
+-- Table `freebed`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freebed`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `pseudo` VARCHAR(45) NULL,
+  `nom` VARCHAR(45) NULL,
+  `prenom` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `password` VARCHAR(255) NULL,
+  `bailleur` TINYINT(1) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `freebed`.`adresses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freebed`.`adresses` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `adresse` VARCHAR(255) NULL,
+  `cp` TINYINT(5) NULL,
+  `ville` VARCHAR(255) NULL,
+  `pays` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `freebed`.`biens`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freebed`.`biens` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NULL,
+  `type` SET('Appartement','Gite', 'Chambre d\'hote') NULL,
+  `surface` VARCHAR(45) NULL,
+  `tarif` DECIMAL(99,99) NULL,
+  `description` MEDIUMTEXT NULL,
+  `users_id` INT NOT NULL,
+  `adresse_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_biens_users_idx` (`users_id` ASC),
+  INDEX `fk_biens_adresse1_idx` (`adresse_id` ASC),
+  CONSTRAINT `fk_biens_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `freebed`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_biens_adresse1`
+    FOREIGN KEY (`adresse_id`)
+    REFERENCES `freebed`.`adresses` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `freebed`.`date_disponibilites`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freebed`.`date_disponibilites` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date_debut` DATETIME NULL,
+  `date_fin` DATETIME NULL,
+  `biens_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_date_disponibilites_biens1_idx` (`biens_id` ASC),
+  CONSTRAINT `fk_date_disponibilites_biens1`
+    FOREIGN KEY (`biens_id`)
+    REFERENCES `freebed`.`biens` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `freebed`.`images`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freebed`.`images` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NULL,
+  `url` VARCHAR(255) NULL,
+  `biens_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_images_biens1_idx` (`biens_id` ASC),
+  CONSTRAINT `fk_images_biens1`
+    FOREIGN KEY (`biens_id`)
+    REFERENCES `freebed`.`biens` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `freebed`.`avis`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freebed`.`avis` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `commentaire` MEDIUMTEXT NULL,
+  `note` TINYINT(2) NULL,
+  `biens_id` INT NOT NULL,
+  `users_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_avis_biens1_idx` (`biens_id` ASC),
+  INDEX `fk_avis_users1_idx` (`users_id` ASC),
+  CONSTRAINT `fk_avis_biens1`
+    FOREIGN KEY (`biens_id`)
+    REFERENCES `freebed`.`biens` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_avis_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `freebed`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `freebed`.`locations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `freebed`.`locations` (
+  `users_id` INT NOT NULL,
+  `biens_id` INT NOT NULL,
+  PRIMARY KEY (`users_id`, `biens_id`),
+  INDEX `fk_users_has_biens_biens1_idx` (`biens_id` ASC),
+  INDEX `fk_users_has_biens_users1_idx` (`users_id` ASC),
+  CONSTRAINT `fk_users_has_biens_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `freebed`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_biens_biens1`
+    FOREIGN KEY (`biens_id`)
+    REFERENCES `freebed`.`biens` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
