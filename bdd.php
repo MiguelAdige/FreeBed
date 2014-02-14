ffa2e2d149bccf5d4553bcf6c90f128879a18549
JuanTrochez <juan.trochez@hotmail.fr>
2014-02-14 17:23:26 +0100

    Modification bdd.phph et connexion.php

--- bdd.php
+++ bdd.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
 
-$bdd = new PDO('mysql:host=localhost;dbname=freebed','root','')
+try{
+	//On se connecte a MySQL
+	$bdd = new PDO('mysql:host=localhost;dbname=freebed','root','');
+}
+catch(Exception $e){
+	//Si erreur, on affiche un message
+	die('Erreur : '.$e->getMessage());
+}
 
 ?>
--- include/connexion.php
+++ include/connexion.php
@@ -1,4 +1,21 @@
 <?php 
+
+	if(isset($_POST["pseudo"])){
+		//creation de la variable du pseudo pour recuperer ce que l'utilisateur a entrer dans le formulaire
+		$pseudo = $_POST["pseudo"];
+		//creation de la variable du mot de passe pour recuperer ce que l'utilisateur a entrer dans le formulaire
+		$password = $_POST["password"];
+		//requete effectuee a la base de donnee pour la connexion (on verifie que le pseudo et le mot de passe corresponde bien à un utilisateur)
+		$resultat = $bdd->exec("SELECT * FROM user WHERE pseudo = ".$pseudo." AND password = ".$password."LIMIT 1;");
+		
+		//on verifie que la requete renvoi une valeur
+		if($resultat != 0){
+
+		}
+		else{
+			echo "Erreur dans le pseudo/password";
+		}
+	}
 	
 	
 
@@ -18,7 +35,7 @@
     			</tr>
 
     			<tr>
-    				<td colspan="2"><input type="submit" value="Se connecter"></td>
+    				<td colspan="2"><input type="submit" value="Se connecter" name="connexion"></td>
     			</tr>
 
     		</table>
