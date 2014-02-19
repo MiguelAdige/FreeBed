88ba7eb84d45cf812cebc6e7a19fa41b8821a7f6
MiguelAdige <mixta971@hotmail.fr>
2014-02-19 11:26:34 +0100

    Correction footer

--- footer.php
+++ footer.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html>
-<link href="footer.css" rel="stylesheet" type="text/css" />
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<body>
 
 <footer>
-  <div id="line">
-  </div>
-  <div id="all">
-  <div id="left">
-   <img src="zoo.jpg" >
- 	</div>
-  <div id="right">
-	<div id="menu" class="border1">
-		<ul>
-			<li class=""><a href="#">Accueil</a></li>
-			<li class=""><a href="#">Haute de page</a></li>		
-		</ul>
-	</div>
-  </div>
-</div>
+  
 </footer>
 
 </body>
