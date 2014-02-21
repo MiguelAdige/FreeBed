0fd6db1a21ab5a296e90817632a65b234075ff1e
sonia <sonia.matthiesen@laposte.net>
2014-02-21 13:51:55 +0100

    header 2
    recherche....

--- header.php
+++ header.php
@@ -8,9 +8,22 @@
 </head>
 <body>
 
+	<nav method="POST" action="login.php"> 
+		Login<input type="text" name="email"/><br/> 
+		Password<input type="password" name="password"/><br/> 
+		<input type="submit" name="submit" value="se connecter"> 
+	</nav>
+
+	<form method="get" action="/recherche.php" onsubmit="var q = $('#q').val(); if(q == 'Recherche' || q == ''){return false;}">
+		<fieldset>
+			<input type="text" id="q" name="q" class="search placeholder" placeholder="Recherche" value="Recherche">
+			<input type="submit" name="submit_search" value="">
+		</fieldset>
+	</form>
+
 	<ul id="menu_horizontal">
 <li class="bouton_gauche"><a href="accueil.php">Accueil</a></li>
-<li class="bouton_gauche"><a href="inscrption.php">Inscrption</a></li>
+<li class="bouton_gauche"><a href="inscription.php">Inscription</a></li>
 <li class="bouton_gauche"><a href="connexion.php">Connexion</a></li>
 <li class="bouton_droite"><a href="contact.php">Contact</a></li>
 	</ul>
