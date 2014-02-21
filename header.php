7c7c6fc8e792e71dcc06fe8e148d7afa447d88d4
sonia <sonia.matthiesen@laposte.net>
2014-02-21 11:09:37 +0100

    Header
    modif

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="header.css">
+  <script src="script.js"></script>
+</head>
+<body>
+
+	<ul id="menu_horizontal">
+<li class="bouton_gauche"><a href="accueil.php">Accueil</a></li>
+<li class="bouton_gauche"><a href="inscrption.php">Inscrption</a></li>
+<li class="bouton_gauche"><a href="connexion.php">Connexion</a></li>
+<li class="bouton_droite"><a href="contact.php">Contact</a></li>
+	</ul>
+
+</body>
+</html>
