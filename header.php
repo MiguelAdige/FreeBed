17518c050fcaac720f2f762d100ccdd82996d0d5
MiguelAdige <mixta971@hotmail.fr>
2014-03-06 12:40:00 +0100

    Modif header

--- header.php
+++ header.php
@@ -7,26 +7,43 @@
 </head>
 <body>
 <header>
+	<?php 
+	if(!$security->logged()){
+	?>
 	<form class="left" method="POST" action="?p=connexion"> 
 		<label for="pseudo">Pseudo</label>
 		<input type="text" name="pseudo"/><br/> 
 		<label for="password">Password</label>
 		<input type="password" name="password"/><br/> 
-		<button type="submit" name="submit">Se connecter</button>
+		<button type="submit">Se connecter</button>
 	</form>
+	<?php
+	}
+	?>
 
-	<form class="right" action="?p=recherche" method="post">
+	<form class="right header-search" action="?p=recherche" method="post">
 		<input type="text" name="recherche"/>
 		<button type="submit">Ok</button>
 	</form>
 
 	<nav>
 		<ul id="menu">
-			<li class="bouton_gauche"><a href="?p=accueil">Accueil</a></li>
-			<li class="bouton_gauche"><a href="?p=inscription">Inscription</a></li>
-			<li class="bouton_gauche"><a href="?p=connexion">Connexion</a></li>
-			<li class="bouton_droite"><a href="?p=recherche">Recherche</a></li>
-			<li class="bouton_droite"><a href="?p=contact">Contact</a></li>
+			<li><a href="?p=accueil">Accueil</a></li>
+			<li><a href="?p=recherche">Recherche</a></li>
+			<li><a href="?p=contact">Contact</a></li>
+			<?php 
+			if(!$security->logged()){
+			?>
+			<li><a href="?p=inscription">Inscription</a></li>
+			<li><a href="?p=connexion">Connexion</a></li>
+			<?php
+			}
+			else{
+			?>
+			<li><a href="?p=logout">Déconnexion</a></li>
+			<?php
+			}
+			?>
 		</ul>
 	</nav>
 </header>
