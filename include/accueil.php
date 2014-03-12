de7769fdbf779053061a41774cba106aab6ba79f
MiguelAdige <mixta971@hotmail.fr>
2014-03-12 23:19:17 +0100

    Mofication de la page d'acceuil
    c'etait pas utile de faire plusieurs pages pour chaque pays le mieux est
    de faire un lien vers la page de recherche qui va chercher par pays

--- include/accueil.php
+++ include/accueil.php
@@ -4,50 +4,50 @@
 <div id="portfolio">
 	<ul class="portfolio-grid">
 					<li><div class="images">
-						<a href="8pays/etat-unis.php">
+						<a href="?p=recherche&pays=etat-unis.php">
 							<div class="countryimage"><img class="taillephoto" src="img/Etat-Unis.jpg" alt="Etat-Unis"/></div>
 							<div class="texte">Etat-Unis</div>
 						</a></div>
 					</li>
 					<li><div class="images">
-						<a href="8pays/allemagne.php">
+						<a href="?p=recherche&pays=allemagne.php">
 							<div class="countryimage"><img class="taillephoto" src="img/Allemagne.jpg" alt="Allemagne"/></div>
 							<div class="texte">Allemagne</div>
 						</a></div>
 					</li>
 					<li><div class="images">
-						<a href="8pays/angleterre.php">
+						<a href="?p=recherche&pays=angleterre">
 							<div class="countryimage"><img class="taillephoto" src="img/Angleterre.jpg" alt="Angleterre"/></div>
 							<div class="texte">Angleterre</div>
 						</a></div>
 					</li>
 					<li><div class="images">
-						<a href="8pays/chine.php">
+						<a href="?p=recherche&pays=chine">
 							<div class="countryimage"><img class="taillephoto" src="img/Chine.jpg" alt="Chine"/></div>
 							<div class="texte">Chine</div>
 						</a></div>
 					</li>
 					<br/>
 					<li><div class="images">
-						<a href="8pays/espagne.php">
+						<a href="?p=recherche&pays=espagne">
 							<div class="countryimage"><img class="taillephoto" src="img/Espagne.jpg" alt="Espagne"/></div>
 							<div class="texte">Espagne</div>
 						</a></div>
 					</li>
 					<li><div class="images">
-						<a href="8pays/france.php">
+						<a href="?p=recherche&pays=france">
 							<div class="countryimage"><img class="taillephoto" src="img/France.jpg" alt="France"/></div>
 							<div class="texte">France</div>
 						</a></div>
 					</li>
 					<li><div class="images">
-						<a href="8pays/turquie.php">
+						<a href="?p=recherche&pays=turquie">
 							<div class="countryimage"><img class="taillephoto" src="img/Turquie.jpg" alt="Turquie"/></div>
 							<div class="texte">Turquie</div>
 						</a></div>
 					</li>
 					<li><div class="images">
-						<a href="8pays/italie.php">
+						<a href="?p=recherche&pays=italie">
 							<div class="countryimage"><img class="taillephoto" src="img/Italie.jpg" alt="Italie"/></div>
 							<div class="texte">Italie</div>
 						</a></div>
